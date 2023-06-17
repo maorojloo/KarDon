95989fc706352c4d02c9d7fae1e15be7defa042c
khoramism <khoramism@gmail.com>
2023-06-17 08:41:44 +0000

    getList() working fine

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 #sqlalchemy modules
-import sqlalchemy as db
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import NormalDoc, Company, CompanyInfo, Province, City, Country, Location,  WorkTypes, JobBoard, ActivationTime, AcademicRequirements, RequiredEducations, ExpireTime, JobPost, 
+#import sqlalchemy as db
+#from sqlalchemy import create_engine
+#from sqlalchemy.orm import sessionmaker
+from models import (NormalDoc, Company, CompanyInfo, Province, City, Country, Location,  WorkTypes, 
+                    JobBoard, ActivationTime, AcademicRequirements, RequiredEducations, ExpireTime,  JobPost)
 #etc
 import requests
 import redis
 from config import redis_auth  
-r = redis()
-redis.Redis(host='localhost', port=6379, password=redis_auth, db=0)
-dburl='sqlite:///kardon.db'
-engine = create_engine(dburl)
+r = redis.Redis(host='localhost', port=6379, password=redis_auth, db=0)
 
 
 
 
 
-def getList(payload, headers):
+def getList():
     url = "https://api.karbord.io/api/v1/Candidate/JobPost/GetList"
     payload = "{\n  \"isInternship\": false,\n  \"isRemote\": false,\n  \"location\": null,\n  \"publishDate\": null,\n  \"workType\": null,\n  \"pageSize\": 5000,\n  \"page\": 1,\n  \"sort\": 0,\n  \"nextPageToken\": \"{\\\"lastJobPostTime\\\":\\\"2023-05-13T09:50:36.993\\\"}\",\n  \"searchId\": null,\n  \"JobPostCategories\": [\n    \"all-Programming\",\n    \"cto\",\n    \"developer\",\n    \"backend-developer\",\n    \"frontend-developer\",\n    \"node-js-developer\",\n    \"php-developer\",\n    \"wordpress-developer\",\n    \"dot-net-developer\",\n    \"vue-js-developer\",\n    \"full-stack-developer\",\n    \"react-developer\",\n    \"python-developer\",\n    \"java-developer\",\n    \"tech-lead\",\n    \"android-developer\",\n    \"ios-developer\",\n    \"flutter-developer\",\n    \"angular-developer\",\n    \"go-developer\",\n    \"c-developer\",\n    \"dba\"\n  ],\n  \"jobBoardIds\": []\n}"
     headers = {'content-type': 'application/json'}
@@ -34,6 +32,7 @@ def getList(payload, headers):
             #print("job number "+str(i)+" job id is "+str(id)+" url is ")
             try:
                 r.set(_id, url)
+                print(_id, url)
             except:
                 print("failed to set in redis")
     else:
@@ -43,9 +42,10 @@ def getList(payload, headers):
 
 
 
-def str2bool(v):
-  return v.lower() in ("yes", "true", "t", "1")
+#getList()
 
+def str2bool(v: str):
+    return v.lower() in ("yes", "true", "t", "1")
 
 def getAndAddJobDetail(jobPostId,url):
     url = "https://api.karbord.io/api/v1/Candidate/JobPost/GetDetail"
@@ -54,26 +54,26 @@ def getAndAddJobDetail(jobPostId,url):
     response = requests.request("GET", url, data=payload, params=querystring)
 
     
-    new_instance = JobDetail(
-        user_id = response.json()['data']['id'],
-        url = response.json()['data']['url'],
-        title = response.json()['data']['title'],
-        description= response.json()['description'],
-        companynametitleFa= response.json()['data']['company']['name']['titleFa'],
-        companylogo =response.json()['data']['logo'],
-        #locations =response.json()['data'],
-        #workTypes = response.json()['data'],
-        hasWorkExperienceRequirement =str2bool(response.json()['data']['hasWorkExperienceRequirement'])? "بدون سابقه کار":"نیاز به سابقه کار",
-        hasAlternativeMilitary = str2bool(response.json()['data']['hasAlternativeMilitary'])? "بدون نیاز به معافیت":"نیاز به معافیت",
-        #benefits = response.json()['data'],
-        seniorityLevel=response.json()['data']['seniorityLevel'],
-        publishTimedate = response.json()['data']['publishTime']['date'],
-        jobBoardorganizationColor = response.json()['data']['jobBoard']['organizationColor'],
-        jobBoardtitleFa = response.json()['data']['jobBoard']['titleFa'],
-        jobBoardtitleEn = response.json()['data']['jobBoard']['titleEn'],
-        companyDetailsSummarynametitleFa = response.json()['data']['companyDetailsSummary']['name']['titleFa']
-        )
+    #new_instance = (
+    #    user_id = response.json()['data']['id'],
+    #    url = response.json()['data']['url'],
+    #    title = response.json()['data']['title'],
+    #    description= response.json()['description'],
+    #    companynametitleFa= response.json()['data']['company']['name']['titleFa'],
+    #    companylogo =response.json()['data']['logo'],
+    #    #locations =response.json()['data'],
+    #    #workTypes = response.json()['data'],
+    #    hasWorkExperienceRequirement =str2bool(response.json()['data']['hasWorkExperienceRequirement']),
+    #    hasAlternativeMilitary = str2bool(response.json()['data']['hasAlternativeMilitary']),
+    #    #benefits = response.json()['data'],
+    #    seniorityLevel=response.json()['data']['seniorityLevel'],
+    #    publishTimedate = response.json()['data']['publishTime']['date'],
+    #    jobBoardorganizationColor = response.json()['data']['jobBoard']['organizationColor'],
+    #    jobBoardtitleFa = response.json()['data']['jobBoard']['titleFa'],
+    #    jobBoardtitleEn = response.json()['data']['jobBoard']['titleEn'],
+    #    companyDetailsSummarynametitleFa = response.json()['data']['companyDetailsSummary']['name']['titleFa']
+    #    )
     
 
-for i in r.keys("*"):
-    getAndAddJobDetail(i.decode('utf-8'), r.get(i).decode('utf-8'))    
+#for i in r.keys("*"):
+#    getAndAddJobDetail(i.decode('utf-8'), r.get(i).decode('utf-8'))    
--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,1 @@
-from .KardonModels import NormalDoc, Company, CompanyInfo, Province, City, Country, Location,  WorkTypes, JobBoard, ActivationTime, AcademicRequirements, RequiredEducations, ExpireTime, JobPost, 
+from .KardonModels import NormalDoc, Company, CompanyInfo, Province, City, Country, Location,  WorkTypes, JobBoard, ActivationTime, AcademicRequirements, RequiredEducations, ExpireTime, JobPost 
