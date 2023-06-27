e76675c0f8a09aa404758f4cc80303040ae70bd7
MoahmmadAminOrojloo <ma.orojloo@gmail.com>
2023-06-27 20:59:29 +0330

    a

--- crawler/tajrobe.py
+++ crawler/tajrobe.py
@@ -59,15 +59,15 @@ for company in companys_list:
          if not redis_client.get(id):
             #inserting to elastic
             doc={                              
-                  "Id" : id
-                  "description" :description 
-                  "rate" : rate
-                  "agent" :agent
-                  "email" :email
-                  "job_name" :job_name
-                  "state" :state
-                  "description" :description 
-                  "cons" : cons
+                  "Id" : id,
+                  "description" :description ,
+                  "rate" : rate,
+                  "agent" :agent,
+                  "email" :email,
+                  "job_name" :job_name,
+                  "state" :state,
+                  "description" :description, 
+                  "cons" : cons,
                   "date" : data
             }
             docs.app(doc)
